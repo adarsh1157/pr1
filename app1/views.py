df8e46f7cbe7e211650c09cb581682ca746c12f3
adarsh1157 <adarsh.keskar_entc22@pccoer.in>
2025-01-14 11:42:04 +0530

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -1,33 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
 import qrcode
 from io import BytesIO
 import base64
-from django.http import HttpResponse
-from django.shortcuts import render
 
-def generate_qr_code(request):
-    if request.method == 'POST':
-        data = request.POST.get('data')
-        
-        if data:
-            # Generate the QR Code
-            qr = qrcode.make(data)
-            buffer = BytesIO()
-            qr.save(buffer, format='PNG')
-            buffer.seek(0)
-            
-            # Convert QR code to base64 for displaying in HTML
-            qr_image_base64 = base64.b64encode(buffer.getvalue()).decode()
+def generate_qr(request):
+    qr_code_image = None
+    if request.method == "POST":
+        qr_data = request.POST.get('qr_data')
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(qr_data)
+        qr.make(fit=True)
 
-            # Store QR code in session history
-            qr_history = request.session.get('qr_history', [])
-            qr_history.append({'data': data, 'qr_image': qr_image_base64})
-            request.session['qr_history'] = qr_history
+        img = qr.make_image(fill_color="black", back_color="white")
+        buffer = BytesIO()
+        img.save(buffer, format='PNG')
+        buffer.seek(0)
+        qr_code_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
-            # Create the downloadable response
-            response = HttpResponse(buffer, content_type='image/png')
-            response['Content-Disposition'] = 'attachment; filename="qrcode.png"'
-            return response
-
-    # Retrieve QR code history from session
-    qr_history = request.session.get('qr_history', [])
-    return render(request, 'QR.html', {'qr_history': qr_history})
+    return render(request, 'generate_qr.html', {'qr_code_image': qr_code_image, 'qr_data': qr_data if request.method == "POST" else None})
