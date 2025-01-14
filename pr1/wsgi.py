4f119c1c65a01521c191395408a43b196f2e253c
adarsh1157 <adarsh.keskar_entc22@pccoer.in>
2025-01-14 10:54:26 +0530

    Update wsgi.py

--- pr1/wsgi.py
+++ pr1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pr1 project.
+WSGI config for qr_generator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pr1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qr_generator.settings')
 
 application = get_wsgi_application()
-app = application
+
+app=application
